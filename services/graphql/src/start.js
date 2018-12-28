06a882862490c4f9ef359c0506db9efff58b2238
Jacob Bare <jacob@limit0.io>
2018-12-28 13:12:34 -0500

    Create account on start up (dev only)

--- services/graphql/src/start.js
+++ services/graphql/src/start.js
@@ -1,6 +1,8 @@
 
 const mongoose = require('./mongoose/connections');
+const Account = require('./mongoose/models/account');
 const { log } = require('./output');
+const { NODE_ENV, ACCOUNT_KEY } = require('./env');
 
 const start = (promise, name, url) => {
   log(`> Connecting to ${name}...`);
@@ -11,7 +13,30 @@ const start = (promise, name, url) => {
   });
 };
 
+const createAccount = async () => new Promise((resolve, reject) => {
+  if (NODE_ENV !== 'development') {
+    resolve();
+  } else {
+    mongoose.core.once('open', () => {
+      Account.findOneAndUpdate({ key: ACCOUNT_KEY }, {
+        $setOnInsert: { key: ACCOUNT_KEY, name: 'Development Account' },
+      }, {
+        upsert: true,
+        setDefaultsOnInsert: true,
+      }, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          log(`> Successfully created account '${ACCOUNT_KEY}'`);
+          resolve();
+        }
+      });
+    });
+  }
+});
+
 module.exports = () => Promise.all([
   start(mongoose.core, 'MongoDB core', m => m.client.s.url),
   start(mongoose.account, 'MongoDB account', m => m.client.s.url),
+  createAccount(),
 ]);
