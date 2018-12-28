e2a1bf7211a6b59289c9b8e0ffd9eafbbbb8f893
Jacob Bare <jacob@limit0.io>
2018-12-28 11:44:10 -0500

    Create mongoose connections

---
+++ services/graphql/src/mongoose/connections/account.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const env = require('../../env');
+const { name, version } = require('../../../package.json');
+
+const { MONGO_DSN, ACCOUNT_KEY } = env;
+
+const instanceDSN = MONGO_DSN.replace('/parcel-plug', `/parcel-plug-${ACCOUNT_KEY}`);
+
+const connection = mongoose.createConnection(instanceDSN, {
+  // autoIndex: env.NODE_ENV !== 'production',
+  appname: `${name} v${version}`,
+  bufferMaxEntries: 0, // Default -1
+  ignoreUndefined: true,
+});
+module.exports = connection;
