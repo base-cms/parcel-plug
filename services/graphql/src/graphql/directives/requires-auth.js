c250eb185a554f1636d8ca6d126214fc10287f01
Jacob Bare <jacob@limit0.io>
2019-01-07 13:41:16 -0500

    Remove setting model user context

--- services/graphql/src/graphql/directives/requires-auth.js
+++ services/graphql/src/graphql/directives/requires-auth.js
@@ -12,17 +12,12 @@ class RequiresAuthDirective extends SchemaDirectiveVisitor {
 
     // eslint-disable-next-line no-param-reassign
     field.resolve = (...args) => {
-      const [doc, , { auth }] = args;
+      const [, , { auth }] = args;
       if (!auth.isValid()) {
         throw new AuthenticationError('You must be logged-in to access this resource.');
       }
       if (role && !auth.hasRole(role)) {
         throw new ApolloError('You do not have permission to access this resource.', 'UNAUTHORIZED');
-      }
-      // Apply user attribution where applicable.
-      // Only works on pre-existing docs (will not work on create).
-      if (doc && typeof doc.setUserContext === 'function') {
-        doc.setUserContext(auth.user);
       }
       if (typeof resolve === 'function') {
         return resolve(...args);
