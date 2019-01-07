6ce9e2737f2042d9bf6d8a1add3e1fbe1454f5cf
Jacob Bare <jacob@limit0.io>
2019-01-07 11:27:05 -0500

    Ensure resolver is applied after auth check

--- services/graphql/src/graphql/directives/auth.js
+++ services/graphql/src/graphql/directives/auth.js
@@ -7,16 +7,19 @@ class AuthDirective extends SchemaDirectiveVisitor {
    * @param {*} field
    */
   visitFieldDefinition(field) {
+    const { resolve } = field;
+    const { requiresRole } = this.args;
+
     // eslint-disable-next-line no-param-reassign
-    field.resolve = async (doc, _, { auth }) => {
-      const { requiresRole } = this.args;
+    field.resolve = function checkAuth(...args) {
+      const [, , { auth }] = args;
       if (!auth.isValid()) {
         throw new AuthenticationError('You must be logged-in to access this resource.');
       }
       if (requiresRole && !auth.hasRole(requiresRole)) {
         throw new ApolloError('You do not have permission to access this resource.', 'UNAUTHORIZED');
       }
-      return doc;
+      return resolve.apply(this, args);
     };
   }
 }
