e4ef08690e93ac85fa14b7a1bef420654a05e1c1
Jacob Bare <jacob@limit0.io>
2019-01-07 12:04:32 -0500

    Create userAttributionPlugin

--- services/graphql/src/mongoose/plugins/index.js
+++ services/graphql/src/mongoose/plugins/index.js
@@ -2,10 +2,12 @@ const deleteablePlugin = require('./deleteable');
 const paginablePlugin = require('./paginable');
 const referencePlugin = require('./reference');
 const repositoryPlugin = require('./repository');
+const userAttributionPlugin = require('./user-attribution');
 
 module.exports = {
   deleteablePlugin,
   paginablePlugin,
   referencePlugin,
   repositoryPlugin,
+  userAttributionPlugin,
 };
---
+++ services/graphql/src/mongoose/plugins/user-attribution.js
@@ -0,0 +1,38 @@
+const { Schema } = require('mongoose');
+const connection = require('../connections/account');
+
+const UserReference = {
+  type: Schema.Types.ObjectId,
+  required: true,
+  validate: {
+    async validator(id) {
+      const doc = await connection.model('user').findById(id, { _id: 1 });
+      if (doc) return true;
+      return false;
+    },
+    message: 'No user found for ID {VALUE}',
+  },
+};
+
+module.exports = function userAttributionPlugin(schema) {
+  schema.add({
+    createdById: UserReference,
+    updatedById: UserReference,
+  });
+
+  schema.method('setUserContext', function setUserContext({ id }) {
+    this.$userContext = { id };
+  });
+
+  schema.pre('validate', function setAttribution(next) {
+    if (this.$userContext) {
+      const { id } = this.$userContext;
+      if (this.isNew) {
+        this.set({ createdById: id, updatedById: id });
+      } else {
+        this.set({ updatedById: id });
+      }
+    }
+    next();
+  });
+};
