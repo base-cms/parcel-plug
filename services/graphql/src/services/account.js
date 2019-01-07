990707bff43602f9bfceef9ad691d25e9c491860
Jacob Bare <jacob@limit0.io>
2019-01-07 08:58:25 -0500

    Create account service

---
+++ services/graphql/src/services/account.js
@@ -0,0 +1,35 @@
+const objectPath = require('object-path');
+const Account = require('../mongoose/models/account');
+const env = require('../env');
+
+module.exports = {
+  /**
+   * Retrieves the account from the database using its key.
+   */
+  async retrieve() {
+    const key = this.getKey();
+    if (!key) throw new Error('Unable to retrieve account: no account key was set.');
+    const account = await Account.findOne({ key });
+    if (!account) throw new Error(`No account found for key '${key}'`);
+    return account;
+  },
+
+  /**
+   * Retrieves an account setting.
+   *
+   * @param {string} path A dot-notated object path/key.
+   */
+  async setting(path) {
+    const account = await this.retrieve();
+    const { settings } = account;
+    return objectPath.get(settings, path);
+  },
+
+  /**
+   * Gets the account key.
+   */
+  getKey() {
+    const { ACCOUNT_KEY } = env;
+    return ACCOUNT_KEY;
+  },
+};
