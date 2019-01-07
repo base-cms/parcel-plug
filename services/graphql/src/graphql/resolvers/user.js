b5c79f59e9182c0fd65b2790825b9f2bfd218cf1
Jacob Bare <jacob@limit0.io>
2019-01-07 10:47:41 -0500

    Add createUser mutation

--- services/graphql/src/graphql/resolvers/user.js
+++ services/graphql/src/graphql/resolvers/user.js
@@ -1,3 +1,31 @@
-module.exports = {
+const User = require('../../mongoose/models/user');
 
+module.exports = {
+  /**
+   *
+   */
+  Mutation: {
+    /**
+     *
+     */
+    createUser: (_, { input }) => {
+      // @todo Check admin auth!
+      const {
+        email,
+        givenName,
+        familyName,
+        password,
+        confirmPassword,
+        role,
+      } = input;
+      User.validatePassword(password, confirmPassword);
+      return User.create({
+        email,
+        givenName,
+        familyName,
+        password,
+        role,
+      });
+    },
+  },
 };
