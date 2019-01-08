35eaca63f9aef70c4b14cc7645fec75ae7af21cb
Jacob Bare <jacob@limit0.io>
2019-01-08 10:25:30 -0500

    Handle error and loading states

--- services/manage/app/routes/application.js
+++ services/manage/app/routes/application.js
@@ -1,6 +1,30 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import ActionMixin from '@base-cms/parcel-plug-manage/mixins/action-mixin';
 
-export default Route.extend(ApplicationRouteMixin, {
+export default Route.extend(ApplicationRouteMixin, ActionMixin, {
   routeAfterAuthentication: 'manage',
+
+  actions: {
+    /**
+     *
+     * @param {*} transition
+     */
+    loading(transition) {
+      this.showLoading();
+      transition.finally(() => this.hideLoading());
+    },
+
+    /**
+     *
+     * @param {Error} e
+     */
+    error(e) {
+      if (this.get('graphErrors').isReady()) {
+        this.get('graphErrors').show(e);
+      } else {
+        this.intermediateTransitionTo('application_error', e);
+      }
+    },
+  },
 });
