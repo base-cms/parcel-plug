1543732a0b650e996f478e7e5bee0b7490680a82
Jacob Bare <jacob@limit0.io>
2019-01-08 09:33:24 -0500

    Create logout route; set auth routes to manage

--- services/manage/app/routes/logout.js
+++ services/manage/app/routes/logout.js
@@ -1,6 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Route.extend(UnauthenticatedRouteMixin, {
-  routeIfAlreadyAuthenticated: 'manage',
+  session: inject(),
+
+  beforeModel() {
+    return this.get('session').invalidate();
+  }
 });
