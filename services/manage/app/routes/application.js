a92156d645da06f25fce9ea233a4b20e2dd825c6
Jacob Bare <jacob@limit0.io>
2019-01-08 09:22:32 -0500

    Setup initial login/auth structure

--- services/manage/app/routes/application.js
+++ services/manage/app/routes/application.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
 import { RouteQueryManager } from 'ember-apollo-client';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 import query from '@base-cms/parcel-plug-manage/gql/ping';
 
-export default Route.extend(RouteQueryManager, {
+export default Route.extend(ApplicationRouteMixin, RouteQueryManager, {
   model() {
     return this.get('apollo').watchQuery({ query });
   },
