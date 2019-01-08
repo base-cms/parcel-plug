563ef99e40b20413f80ce7ad63137449581bec8b
Jacob Bare <jacob@limit0.io>
2019-01-08 09:09:46 -0500

    Change gql import

--- services/manage/app/routes/application.js
+++ services/manage/app/routes/application.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import { RouteQueryManager } from 'ember-apollo-client';
 
-import query from '../gql/ping';
+import query from '@base-cms/parcel-plug-manage/gql/ping';
 
 export default Route.extend(RouteQueryManager, {
   model() {
