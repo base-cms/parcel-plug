aefb5f20d990c57aee121b4eef8fecda2eece3bd
Jacob Bare <jacob@limit0.io>
2019-01-08 09:22:01 -0500

    Set rootURL to /app

--- services/manage/config/environment.js
+++ services/manage/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: '@base-cms/parcel-plug-manage',
     environment,
-    rootURL: '/',
+    rootURL: '/app',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
