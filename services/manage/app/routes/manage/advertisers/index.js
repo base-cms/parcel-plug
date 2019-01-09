bb6abaade2d2a0a0227c4f0fb348d7fc0ce2d1e6
Jacob Bare <jacob@limit0.io>
2019-01-09 15:24:49 -0500

    Handle advertiser search/match

--- services/manage/app/routes/manage/advertisers/index.js
+++ services/manage/app/routes/manage/advertisers/index.js
@@ -2,17 +2,20 @@ import Route from '@ember/routing/route';
 import ListRouteMixin from '@base-cms/parcel-plug-manage/mixins/list-route-mixin';
 
 import query from '@base-cms/parcel-plug-manage/gql/queries/advertiser/list';
+import search from '@base-cms/parcel-plug-manage/gql/queries/advertiser/match';
 
 export default Route.extend(ListRouteMixin, {
   /**
    *
    * @param {object} params
    */
-  model({ limit, field, order }) {
+  model({ limit, field, order, phrase, searchType, searchBy }) {
     return this.getResults({
       query,
       queryKey: 'advertisers',
-    }, { limit, field, order });
+      search,
+      searchKey: 'matchAdvertisers',
+    }, { limit, field, order, phrase, searchType, searchBy });
   },
 });
 
