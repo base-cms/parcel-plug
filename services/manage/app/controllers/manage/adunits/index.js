07859cf7df9e4931af5a363141c0fc5ad2a7f7cf
Jacob Bare <jacob@limit0.io>
2019-01-10 13:15:57 -0500

    Add id and createdAt sort options

--- services/manage/app/controllers/manage/adunits/index.js
+++ services/manage/app/controllers/manage/adunits/index.js
@@ -4,6 +4,8 @@ export default ListController.extend({
   init() {
     this._super(...arguments);
     this.set('sortOptions', [
+      { key: 'id', label: 'ID' },
+      { key: 'createdAt', label: 'Created' },
       { key: 'updatedAt', label: 'Updated' },
       { key: 'name', label: 'Name' },
       { key: 'size', label: 'Size' },
