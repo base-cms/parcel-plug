42bb00c904c4d21f6e19b665deb4ad74c17ff560
Jacob Bare <jacob@limit0.io>
2019-01-10 12:59:59 -0500

    Create order list page

---
+++ services/manage/app/controllers/manage/orders/index.js
@@ -0,0 +1,19 @@
+import ListController from '../../abstract-list';
+
+export default ListController.extend({
+  init() {
+    this._super(...arguments);
+    this.set('sortOptions', [
+      { key: 'updatedAt', label: 'Updated' },
+      { key: 'name', label: 'Name' },
+    ]);
+    this.set('field', 'updatedAt');
+    this.set('order', 'desc');
+
+    this.set('searchFields', [
+      { key: 'name', label: 'Name' },
+      { key: 'advertiserName', label: 'Advertiser Name' },
+    ]);
+    this.set('searchBy', 'name');
+  },
+});
