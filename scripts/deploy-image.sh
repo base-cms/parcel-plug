9b5be8a317625095c9d65caa1162c52cae6549bb
Josh Worden <solocommand@gmail.com>
2019-01-14 14:14:58 -0600

    Build the right thing :)

--- scripts/deploy-image.sh
+++ scripts/deploy-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t "base-cms:$1" .
-docker tag "base-cms:$1" "basecms/parcel-plug:$1"
+docker build -t "parcel-plug:$1" .
+docker tag "parcel-plug:$1" "basecms/parcel-plug:$1"
 docker push "basecms/parcel-plug:$1"
