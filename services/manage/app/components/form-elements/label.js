d328711b8b6f7b9b451f2e44948d1ed958a8a1cf
Jacob Bare <jacob@limit0.io>
2019-01-15 14:45:40 -0600

    Create label form element

---
+++ services/manage/app/components/form-elements/label.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  tagName: 'label',
+  attributeBindings: [
+    'for',
+  ],
+
+  required: false,
+  value: '',
+});
