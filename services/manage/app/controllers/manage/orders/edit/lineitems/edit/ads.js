18cdb285cb75a50260713d3fca86e4702e4bfc13
Jacob Bare <jacob@limit0.io>
2019-01-15 10:38:54 -0600

    Create initial lineitem edit ads

---
+++ services/manage/app/controllers/manage/orders/edit/lineitems/edit/ads.js
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  // init() {
+  //   this._super(...arguments);
+  //   this.set('sortOptions', [
+  //     { key: 'id', label: 'ID' },
+  //     { key: 'createdAt', label: 'Created' },
+  //     { key: 'updatedAt', label: 'Updated' },
+  //     { key: 'name', label: 'Name' },
+  //     { key: 'advertiserName', label: 'Advertiser Name' },
+  //   ]);
+  //   this.set('field', 'updatedAt');
+  //   this.set('order', 'desc');
+
+  //   this.set('searchFields', [
+  //     { key: 'name', label: 'Name' },
+  //     { key: 'advertiserName', label: 'Advertiser Name' },
+  //   ]);
+  //   this.set('searchBy', 'name');
+  // },
+});
---
+++ services/manage/app/routes/manage/orders/edit/lineitems/edit/ads.js
@@ -0,0 +1,42 @@
+import Route from '@ember/routing/route';
+import RouteObservableMixin from '@base-cms/parcel-plug-manage/mixins/route-observable-mixin';
+
+import query from '@base-cms/parcel-plug-manage/gql/queries/ad/list-for-lineitem';
+// import search from '@base-cms/parcel-plug-manage/gql/queries/ad/match-for-lineitem';
+
+export default Route.extend(RouteObservableMixin, {
+  /**
+   *
+   * @param {object} params
+   */
+  async model() {
+    const input = {
+      lineitemId: this.modelFor('manage.orders.edit.lineitems.edit').id,
+    };
+    const variables = { input };
+    const queryKey = 'adsForLineItem';
+    this.getController().set('resultKey', queryKey);
+    try {
+      const response = await this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, queryKey);
+      this.getController().set('observable', this.getObservable(response));
+      return response;
+    } catch (e) {
+      this.get('graphErrors').show(e);
+    }
+  },
+  // model({ limit, field, order, phrase, searchType, searchBy }) {
+  //   const input = {
+  //     lineitemId: this.modelFor('manage.orders.edit.lineitems.edit').id,
+  //   };
+  //   return this.getResults({
+  //     query,
+  //     queryKey: 'adsForLineItem',
+  //     queryInput: input,
+  //     search,
+  //     searchKey: 'matchAdsForLineItem',
+  //     searchInput: input,
+  //   }, { limit, field, order, phrase, searchType, searchBy });
+  // },
+});
+
+
