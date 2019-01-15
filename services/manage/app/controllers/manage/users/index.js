95ec9c96c158dcfb8ff8adc268f7cf483110ab96
Josh Worden <solocommand@gmail.com>
2019-01-15 12:12:14 -0600

    Ember support for user crud

---
+++ services/manage/app/controllers/manage/users/index.js
@@ -0,0 +1,20 @@
+import ListController from '../../abstract-list';
+
+export default ListController.extend({
+  init() {
+    this._super(...arguments);
+    this.set('sortOptions', [
+      { key: 'id', label: 'ID' },
+      { key: 'createdAt', label: 'Created' },
+      { key: 'updatedAt', label: 'Updated' },
+      { key: 'name', label: 'Name' },
+    ]);
+    this.set('field', 'updatedAt');
+    this.set('order', 'desc');
+
+    this.set('searchFields', [
+      { key: 'name', label: 'Name' },
+    ]);
+    this.set('searchBy', 'name');
+  },
+});
