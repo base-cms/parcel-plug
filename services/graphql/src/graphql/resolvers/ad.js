d549b976a83304c8a91ec1f0751fe0cbce65f533
Jacob Bare <jacob@limit0.io>
2019-01-16 12:18:44 -0600

    Create the adImage resolver

--- services/graphql/src/graphql/resolvers/ad.js
+++ services/graphql/src/graphql/resolvers/ad.js
@@ -1,3 +1,18 @@
-module.exports = {
+const Ad = require('../../mongoose/models/ad');
 
+module.exports = {
+  /**
+   *
+   */
+  Mutation: {
+    /**
+     *
+     */
+    adImage: async (_, { input }, { auth }) => {
+      const { id, value: file } = input;
+      const ad = await Ad.strictFindActiveById(id);
+      ad.setUserContext(auth.user);
+      return ad.uploadImage(file);
+    },
+  },
 };
