e76e57e96e1945a10cc5b37c943bb86a8825373d
Jacob Bare <jacob@limit0.io>
2019-01-16 10:51:31 -0600

    Use native promise for ping

--- services/graphql/src/s3/ping.js
+++ services/graphql/src/s3/ping.js
@@ -1,12 +1,4 @@
 const s3 = require('./client');
 const { S3_BUCKET: Bucket } = require('../env');
 
-module.exports = () => new Promise((resolve, reject) => {
-  s3.headBucket({ Bucket }, (err, data) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(data);
-    }
-  });
-});
+module.exports = () => s3.headBucket({ Bucket }).promise();
