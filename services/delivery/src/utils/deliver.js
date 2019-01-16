b715014856451c26fa22fb7508f8c45c95e10004
Josh Worden <solocommand@gmail.com>
2019-01-16 11:32:14 -0600

    Support placeholder=true

--- services/delivery/src/utils/deliver.js
+++ services/delivery/src/utils/deliver.js
@@ -1,15 +1,24 @@
 const { mongo } = require('../connections');
 
-module.exports = ({ _id, width, height }, type, { date, email, rand }) => {
+module.exports = ({ _id, width, height }, type, query) => {
+  const { date, email, rand } = query;
+  if (query.placeholder) {
+    return {
+      src: `https://placehold.it/${width}x${height}`,
+      url: 'https://email-x.io',
+    };
+  }
+
   const correlator = Buffer.from(JSON.stringify({ date, email, rand })).toString('base64');
 
   // Write the click/view event
-  mongo.db().collection('delivery-event').insertOne({ correlator, type, adunit: _id }, { upsert: true });
+  mongo.db().collection('delivery-event').updateOne({ correlator, type, adunit: _id }, { $inc: { count: 1 } }, { upsert: true });
 
-  // return mongo.db().collection('delivery-query').findOne({ adunit: _id, width, height, date });
-
-  return {
-    src: `https://placehold.it/${width}x${height}`,
-    url: 'https://email-x.io',
-  };
+  // Return the winning creative
+  return mongo.db().collection('delivery-query').findOne({
+    adunit: _id,
+    width,
+    height,
+    date,
+  });
 };
