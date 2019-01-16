cfd8af10e2e6b2ccc75f1f6af1b065c012c21aea
Jacob Bare <jacob@limit0.io>
2019-01-16 12:41:25 -0600

    Use upload link within apollo service

--- services/manage/app/services/apollo.js
+++ services/manage/app/services/apollo.js
@@ -3,15 +3,17 @@ import { inject } from '@ember/service';
 import authorize from '@base-cms/parcel-plug-manage/apollo/authorize';
 import { computed } from '@ember/object';
 import { setContext } from 'apollo-link-context';
+import { createUploadLink } from 'apollo-upload-client';
 
 export default ApolloService.extend({
   session: inject(),
 
   link: computed(function() {
-    const httpLink = this._super(...arguments);
+    const uri = this.get('apiURL');
+    const uploadLink = createUploadLink({ uri });
     const authLink = setContext((req, ctx) => {
       return authorize(this.get('session'), req, ctx);
     });
-    return authLink.concat(httpLink);
+    return authLink.concat(uploadLink);
   }),
 });
