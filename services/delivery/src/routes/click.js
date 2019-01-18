97167501ebbbfc8acb53bb56c0526cf643ab3594
Jacob Bare <jacob@limit0.io>
2019-01-18 08:59:43 -0600

    Change route param to : adunitid

--- services/delivery/src/routes/click.js
+++ services/delivery/src/routes/click.js
@@ -2,8 +2,9 @@ const { asyncRoute, getAdUnit } = require('../utils');
 const deliver = require('../deliver');
 
 module.exports = (app) => {
-  app.get('/click/:id', asyncRoute(async ({ params, query }, res) => {
-    const adunit = await getAdUnit(params.id);
+  app.get('/click/:adunitid', asyncRoute(async ({ params, query }, res) => {
+    const { adunitid } = params;
+    const adunit = await getAdUnit(adunitid);
 
     const winner = await deliver(adunit, 'click', query);
     if (!winner) return res.status(204).send();
--- services/delivery/src/routes/image.js
+++ services/delivery/src/routes/image.js
@@ -2,8 +2,9 @@ const { asyncRoute, getAdUnit } = require('../utils');
 const deliver = require('../deliver');
 
 module.exports = (app) => {
-  app.get('/image/:id', asyncRoute(async ({ params, query }, res) => {
-    const adunit = await getAdUnit(params.id);
+  app.get('/image/:adunitid', asyncRoute(async ({ params, query }, res) => {
+    const { adunitid } = params;
+    const adunit = await getAdUnit(adunitid);
 
     const winner = await deliver(adunit, 'view', query);
     if (!winner) return res.status(200).send();
