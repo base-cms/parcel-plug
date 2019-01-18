09e54a68c595e19bc38d95e90138d8b84ff393fc
Jacob Bare <jacob@limit0.io>
2019-01-18 09:18:41 -0600

    Use strictFindActiveById

--- services/delivery/src/utils/get-ad-unit.js
+++ services/delivery/src/utils/get-ad-unit.js
@@ -1,9 +1,5 @@
-const { ObjectId } = require('mongodb');
 const db = require('../db');
 
-module.exports = async id => db.strictFindOne('adunits', {
-  _id: new ObjectId(id),
-  deleted: false,
-}, {
+module.exports = id => db.strictFindActiveById('adunits', id, {
   projection: { width: 1, height: 1 },
 });
