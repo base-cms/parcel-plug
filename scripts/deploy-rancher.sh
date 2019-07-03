4da4da6bde70932a0b1a31317129a7815069b2ba
Josh Worden <solocommand@gmail.com>
2019-07-03 09:28:44 -0500

    Add New Relic to parcel plug services

--- scripts/deploy-rancher.sh
+++ scripts/deploy-rancher.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
+./scripts/deploy-notify-start.sh
 yarn global add @base-cms/travis-rancher-deployer
 
 deploy-to-rancher "nginx:alpine" manage-nginx
 deploy-to-rancher "basecms/parcel-plug:${TRAVIS_TAG}" manage
-
 deploy-to-rancher "basecms/parcel-plug:${TRAVIS_TAG}" graphql
 deploy-to-rancher "basecms/parcel-plug:${TRAVIS_TAG}" delivery
+
+./scripts/deploy-notify.sh
