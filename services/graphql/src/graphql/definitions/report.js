fc9335357c843f7aa09d2f754d1b86963af2acae
Josh Worden <solocommand@gmail.com>
2020-01-09 12:29:59 -0600

    Convert ReportRow type to use refOne directives

--- services/graphql/src/graphql/definitions/report.js
+++ services/graphql/src/graphql/definitions/report.js
@@ -11,13 +11,13 @@ type Report {
 }
 
 type ReportRow {
-  advertiser: Advertiser!
-  order: Order!
-  lineitem: LineItem!
-  ad: Ad!
-  publisher: Publisher!
-  deployment: Deployment!
-  adunit: AdUnit!
+  advertiser: Advertiser! @refOne(modelName: "advertiser", localField: "advertiserId", foreignField: "_id")
+  order: Order! @refOne(modelName: "order", localField: "orderId", foreignField: "_id")
+  lineitem: LineItem! @refOne(modelName: "lineitem", localField: "lineitemId", foreignField: "_id")
+  ad: Ad! @refOne(modelName: "ad", localField: "adId", foreignField: "_id")
+  publisher: Publisher! @refOne(modelName: "publisher", localField: "publisherId", foreignField: "_id")
+  deployment: Deployment! @refOne(modelName: "deployment", localField: "deploymentId", foreignField: "_id")
+  adunit: AdUnit! @refOne(modelName: "adunit", localField: "adunitId", foreignField: "_id")
   impressions: Int!
   clicks: Int!
   ctr: Float!
